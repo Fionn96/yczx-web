932dfee5848ff940e1da354cb76e4e24f77c7445
Fionn96 <fyx2500253355@gmail.com>
2024-08-17 18:35:55 +0800

    更新

--- web/next.config.js
+++ web/next.config.js
@@ -1,17 +1,17 @@
+const MillionLint = require('@million/lint');
 const withNextra = require("nextra")({
-	theme: "nextra-theme-blog",
-	themeConfig: "./theme.config.js",
-	staticImage: true,
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
+  staticImage: true
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
-	distDir: 'out',
-	images: {
-		loader: 'imgix',
-		path: '',
-	}
+  output: 'export',
+  distDir: 'out',
+  images: {
+    loader: 'imgix',
+    path: ''
+  }
 };
-
-module.exports = withNextra(nextConfig);
+module.exports = MillionLint.next()(withNextra(nextConfig));
